a11db2deffcc831b5e18652f40e38867e15492ce
Sanil <sanilksnrp@gmail.com>
2023-04-26 14:09:13 +0100

    guarded array timestamps added

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Customer extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
+    protected $guarded = ['id','created_at','updated_at'];
 
     public function user(): BelongsTo
     {
